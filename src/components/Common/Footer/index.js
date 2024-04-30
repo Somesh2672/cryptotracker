ac3b53e4f1a5a9e6a2b5b2bd702ccc7d43ae64b9
Somesh Jaiswal <someshjaiswal98@gmail.com>
2024-04-30 19:52:11 +0530

    cryptoTracker

---
+++ src/components/Common/Footer/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+
+function Footer(){
+    return(
+        <div className="footer">
+            Footer
+        </div>
+    )
+};
+export default Footer;
