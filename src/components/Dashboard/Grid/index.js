5ded120e72b45b7e3b2f863ae7aed23bbb97aa54
Somesh Jaiswal <someshjaiswal98@gmail.com>
2024-08-15 17:02:35 +0530

    gridfunc

--- src/components/Dashboard/Grid/index.js
+++ src/components/Dashboard/Grid/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import "./Style.css";
 import TrendingUpRoundedIcon from '@mui/icons-material/TrendingUpRounded';
 import TrendingDownRoundedIcon from '@mui/icons-material/TrendingDownRounded';
+import { color } from "framer-motion";
 
 
 function Grid({ coin }){
-    return<div className="grid-container">
+    return(<div className={`grid-container ${coin.price_change_percentage_24h < 0 ? "grid-container-red" : ""}`}>
+
         <div className="info-flex">
             <img src={coin.image} className="coin-logo"/>
             <div className="name-col">
@@ -13,19 +15,26 @@ function Grid({ coin }){
                 <p className="coin-name">{coin.name}</p>
             </div>
         </div>
-        {coin.market_cap_change_percentage_24h > 0 ? (
+        {coin.price_change_percentage_24h > 0 ? (
         <div className="chip-flex">
-             <div className="price-chip">{coin.market_cap_change_percentage_24h.toFixed(2)}%</div>
+             <div className="price-chip">{coin.price_change_percentage_24h.toFixed(2)}%</div>
              <div className="icon-chip"><TrendingUpRoundedIcon/></div>
         </div>
         
 
         ):(
         <div className="chip-flex">
-            <div className="price-chip chip-red">{coin.market_cap_change_percentage_24h.toFixed(2)}%</div>
+            <div className="price-chip chip-red">{coin.price_change_percentage_24h.toFixed(2)}%</div>
             <div className="icon-chip chip-red"><TrendingDownRoundedIcon/></div>
        </div>
     )}  
-    </div> ; 
+    <div className="info_container">
+        <h3 className="coin-price" style={{color: coin.price_change_percentage_24h > 0 ? "var(--green)": "var(--red)", }}>${coin.current_price.toLocaleString()}</h3>
+        <p className="total_volume"> Total Volume : {coin.total_volume.toLocaleString()}</p>
+        <p className="market_cap"> Market Cap : ${coin.market_cap.toLocaleString()}</p>
+    </div>
+    
+    </div> ); 
+    
 }
 export default Grid;
